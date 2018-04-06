228f22bd10eb1ce5cb40c1ed5ca56be64b2804c2
Félix Boulet <felix.boulet@polymtl.ca>
2018-04-06 16:45:41 -0400

    Avancement de la détection

--- INF8770_TP3/scene_detector.py
+++ INF8770_TP3/scene_detector.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 # Inspiration : https://bcastell.com/posts/scene-detection-tutorial-part-1/
 def main():
+    THRESHOLD = 8000
+
     video = cv2.VideoCapture("Toots.avi")
 
     if not video.isOpened():
@@ -19,11 +21,14 @@ def main():
 
     last_hist = []
     diff = []
+    thresh_exceed = []
+    cnt = 0
     while True:
         (rv, im) = video.read()
         if not rv:
             break
-        if len(last_hist) == 0 :
+        cnt += 1
+        if len(last_hist) == 0:
             last_bhist, _ = np.histogram(im[:, :, 0].flatten(), 16, (0, 255))
             last_ghist, _ = np.histogram(im[:, :, 1].flatten(), 16, (0, 255))
             last_rhist, _ = np.histogram(im[:, :, 2].flatten(), 16, (0, 255))
@@ -33,18 +38,22 @@ def main():
             ghist, _ = np.histogram(im[:, :, 1].flatten(), 16, (0, 255))
             rhist, _ = np.histogram(im[:, :, 2].flatten(), 16, (0, 255))
             hist = np.concatenate((bhist, ghist, rhist))
-            diff.append(np.linalg.norm(last_hist - hist))
+            difference = np.linalg.norm(last_hist - hist)
+            if difference > THRESHOLD:
+                thresh_exceed.append(difference)
+            else:
+                thresh_exceed.append(None)
+
+            diff.append(difference)
             last_hist = hist
 
-    plt.plot(diff)
+    plt.plot(diff, 'b-', thresh_exceed, 'rx')
     plt.xlabel('Frame number')
     plt.ylabel('Difference')
     plt.show()
 
     frame_count = video.get(cv2.CAP_PROP_POS_FRAMES)
     print("Read %d frames from video." % frame_count)
-
-
     video.release()
 
 
